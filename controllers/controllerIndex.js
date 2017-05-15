14ba0c7c810d08bfbe51e4522214c7da008f86fe
Zachary Lai <tvbatvtv2@hotmail.com>
2017-05-15 18:04:22 +0800

    [Feature] Check in simple file structure: model, view, controller

---
+++ controllers/controllerIndex.js
@@ -0,0 +1,4 @@
+var app = angular.module('myApp', []);
+app.controller('controllerIndex', function($scope) {
+    $scope.message = 'Oh my God!';
+});
--- express.js
+++ express.js
@@ -1,10 +1,20 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var cons = require('consolidate');
+
+
+var app = express();
+
+app.engine('html', cons.swig);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views/server');
+
+app.use(express.static('controllers'));
+app.use(express.static('bower_components'));
 
 app.get('/', function(req, res){
-    res.send('hh');
+    res.render('layout', {'title': 'index'});
 });
 
 app.listen(3001, function(){
-    console.log('Example app listening on port 3001');
+    console.log('app listening on port 3001');
 });
